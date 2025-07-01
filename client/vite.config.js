aef36f125e0136bc8bc014ed8c3d7d672bfebf9a
Max <mq80140@gmail.com>
2025-07-01 06:50:17 +0500

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5000',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios'], // Add more libraries here if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Optional: raises the limit from 500 KB to 1000 KB
+  },
 });
